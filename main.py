9d4c54364c0b5f6f879fe5b5bef20f6e0bdc1106
S Daut <92053137+NameUser5@users.noreply.github.com>
2022-02-09 10:39:49 +0000

    What I have so far. Need to fix the operations

--- main.py
+++ main.py
@@ -1,119 +1,123 @@
-section1 = "The UNBREAKABLE Calculator:"
-section1 = section1.upper()
+title = "The UNBREAKABLE Calculator Ver. 2:"
+title = title.upper()
 
-print(section1)
+print(title)
 
-print("\nWelcome! Feast your eyes upon the great Calcu-maton! Enter two numbers below and prepare to be amazed!~ \n")
+print("\nWelcome! Feast your eyes upon our newly improved wonder, the Calcu-maton 5000! Enter two numbers below and prepare to be amazed!~ \n")
 
-screen = True
-
-while screen == True:
-  
-  calc_run = []
-  try:
-    start = str(input("Do you want to use the calcu-maton? \n <YES>    <NO> \n")).upper()
-    if start == "YES":
-      calc_run = True
-  except start != "YES": 
-    print("Goodbye")
-    # if start != "NO":
-    #   screen == False
-    #   calc_run == False
-    #   print("Alas, now is not the time for trickery. Goodbye and have a jolly good day!")
-    # if start == "NO":
-    #   screen == False
-    #   calc_run == False
-    #   print("Alas, goodbye.")
-      
-  while calc_run == False:
-    if start != "NO":
-      screen == False
-      calc_run == False
-      print("Alas, now is not the time for trickery. Goodbye and have a jolly good day!")
-    if start == "NO":
-      screen == False
-      calc_run == False
-      print("Alas, goodbye.")
-
-  while calc_run == True:
-
-    operations = ["+","-","*","/"]
-
-    def add(num1,num2):
-      total = num1 + num2
-      return total
-    def subtract(num1,num2):
-      total = num1 - num2
-      return total
-    def multiply(num1,num2):
-      total = num1 * num2
-      return total
-    def divide(num1,num2):
-      total = num1 / num2
-      return total
-      
-    
+start = str(input("Do you want to use the Calcu-maton 5000? \n <YES>    <NO> \n")).upper()
+if start == "YES":
+  num1_ask = True
+#   calc_run = True
+  while num1_ask == True:
+    # num1 = ""
     try:
       num1 = float(input("Enter your first number: ")) 
       valid_num1 = True
     except ValueError:
-      print("Nice try.")
+      print("Nice try, fellow.")
       valid_num1 = False
-      continue
+    if valid_num1 == True:
+      num1_ask = False
+      calc_run = True
+else:
+    num1_ask = False
+    calc_run = False
+    num2_ask = False
 
+    print("Goodbye.")
+
+
+
+# num1 = ()
+
+# for n in num1:
+#     if num1_ask == True:
+#     # num1 = ""
+#         try:
+#           num1 = float(input("Enter your first number: ")) 
+#           valid_num1 = True
+#           if valid_num1 == True:
+#             calc_run = True
+#         except ValueError:
+#           print("Nice try, fellow.")
+#           valid_num1 = False
+
+
+
+while calc_run == True:
+
+  operations = ["+","-","*","/"]
+
+  def add(num1,num2):
+    total = num1 + num2
+    return total
+  def subtract(num1,num2):
+    total = num1 - num2
+    return total
+  def multiply(num1,num2):
+    total = num1 * num2
+    return total
+  def divide(num1,num2):
+    total = num1 / num2
+    return total
+  
+  while calc_run == True:
     operation = str(input("Enter your operation: "))
     '''operations = ["+","-","*","/"]'''
     for o in operations:
       if operation in operations:
         valid_opp = True
+        num2_ask = True
       else:
         valid_opp = False
 
-    while valid_opp == False:
-      operation = str(input("Try again. Enter an appropriate symbol. "))
-      
-      if operation in operations:
-        valid_opp = True
+      while valid_opp == False:
+        operation = str(input("Try again. Enter an appropriate symbol. "))
+        
+        if operation in operations:
+          valid_opp = True
+          num2_ask = True
+  
+              #   while num2_ask == True:
+          if num2_ask == True:
+            try:
+              num2 = float(input("Enter your second number: ")) 
+              valid_num2 = True
+              num2_ask = False
+            except ValueError:
+              print("Egads! What are you doing? Try again.")
+              valid_num2 = False
+              while valid_num2 == False:
+                  
+                  try:
+                    num2 = float(input("Enter your second number: ")) 
+                    valid_num2 = True
+                    num2_ask = False
+                  except ValueError:
+                    print("Egads! What are you doing? Try again.")
+                    valid_num2 = False
+                    continue
     
-    try:
-      num2 = float(input("Enter your second number: ")) 
-      valid_num2 = True
-    except ValueError:
-      print("Nice try.")
-      valid_num2 = False
-    while valid_num2 == False:
-      
-      try:
-        num2 = float(input("Enter your second number: ")) 
-        valid_num2 = True
-      except ValueError:
-        print("Nice try.")
-        valid_num2 = False
+#   get_result = True
+
+            for o in operations:
+              if operation == "+":
+                result = add(num1,num2)
+              elif operation == "-":
+                result = subtract(num1,num2)
+              elif operation == "*":
+                result = multiply(num1,num2)
+              elif operation == "/":
+                if num2 != 0:
+                  result = divide(num1,num2)
+                else: 
+                  result = "Undefined. You cannot divide by zero"
+          print(f"\nYour total is: {result}.\n")
+          keep_play = str(input("Do you want to continue using the Calcu-maton 5000? \n <YES>    <NO> \n")).upper()
+          if keep_play == "YES":
+            calc1_run = True
+            num1 = result
+          else:
+            calc_run = False
         continue
-      
-
-    for o in operations:
-      if operation == "+":
-        result = add(num1,num2)
-      elif operation == "-":
-        result = subtract(num1,num2)
-      elif operation == "*":
-        result = multiply(num1,num2)
-      elif operation == "/":
-        if num2 != 0:
-          result = divide(num1,num2)
-        else: 
-          result = "Undefined. You cannot divide by zero"
-      
-    print("\n",result,"\n")
-
-    def setvalue():
-      global num1
-      if result == "Undefined. You cannot divide by zero":
-        num1 = []
-      else:
-        num1 = result
-      return num1
-    continue
-
-
